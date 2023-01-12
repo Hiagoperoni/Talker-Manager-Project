51daa0d04115ad875e1abf058f91dabe0d8cba9e
HiagoP <hiagoperonip@gmail.com>
2023-01-11 22:01:13 -0300

    Requisito 04 completo

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -19,10 +19,22 @@ const gerarToken = () => {
 
 loginRouter.post('/login', (req, res) => {
     const { email, password } = req.body;
-    gerarToken();
-    if (!email || !password) {
-        return res.status(401).json({ message: 'Bad Request' });
+    const regEmail = /\S+@\S+\.\S+/;
+    const validEmail = regEmail.test(email);
+
+    if (!email) {
+        return res.status(400).json({ message: 'O campo "email" é obrigatório' });
     }
+    if (validEmail === false) {
+        return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    }
+    if (!password) {
+        return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    }
+    if (password.length < 6) {
+        return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    }
+    gerarToken();
     return res.status(200).json({ token });
 });
 
