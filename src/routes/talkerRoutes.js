0a12c4243b8e458c75d3a2a7f169db32e00d713e
HiagoP <hiagoperonip@gmail.com>
2023-01-11 23:04:10 -0300

    Requisito 05

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const { readFile } = require('../utils/fs/readData');
+// const { writeFile } = require('../utils/fs/writeData');
 
 const talkerRouter = express.Router();
 const filePath = path.resolve('src', 'talker.json');
+const resultVerify = [0, ''];
 
 talkerRouter.get('/talker', async (req, res) => {
     const data = await readFile(filePath);
@@ -20,8 +22,83 @@ talkerRouter.get('/talker/:id', async (req, res) => {
     return res.status(200).json(result);
 });
 
-// talkerRouter.post('/talker', async (req, res) => {
+const verifyNameAndAge = (name, age) => {
+    if (!name) {
+        resultVerify[0] = 1;
+        resultVerify[1] = 'O campo "name" é obrigatório';
+    }
+    if (name.length < 3) {
+        resultVerify[0] = 1;
+        resultVerify[1] = 'O "name" deve ter pelo menos 3 caracteres';
+    }
+    if (!age) {
+        resultVerify[0] = 1;
+        resultVerify[1] = 'O campo "age" é obrigatório';
+    }
+    if (age < 18) {
+        resultVerify[0] = 1;
+        resultVerify[1] = 'A pessoa palestrante deve ser maior de idade';
+    }
+    return resultVerify;
+};
 
-// });
+const verifyWatchedAt = (watchedAt) => {
+    if (!watchedAt) {
+        resultVerify[0] = 1;
+        resultVerify[1] = 'O campo "watchedAt" é obrigatório';
+    }
+    if (watchedAt instanceof !Date) {
+        resultVerify[0] = 1;
+        resultVerify[1] = 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"';
+    }
+    return resultVerify;
+};
+
+const verifyRate = (rate) => {
+    if (!rate) {
+        resultVerify[0] = 1;
+        resultVerify[1] = 'O campo "rate" é obrigatório';
+    }
+    if (rate < 1 || rate > 5 || Number.isInteger(rate)) {
+        resultVerify[0] = 1;
+        resultVerify[1] = 'O campo "rate" deve ser um inteiro de 1 à 5';
+    }
+    return resultVerify;
+};
+
+const verifyTalk = (talk) => {
+    if (!talk) {
+        resultVerify[0] = 1;
+        resultVerify[1] = 'O campo "talk" é obrigatório';
+    }
+    return resultVerify;
+};
+
+const verifyAuthorization = (authorization) => {
+    if (!authorization) {
+        resultVerify[0] = 1;
+        resultVerify[1] = 'Token não encontrado';
+    }
+    if (authorization.length < 16 || typeof (authorization) !== 'string') {
+        resultVerify[0] = 1;
+        resultVerify[1] = 'Token inválido';
+    }
+    return resultVerify;
+};
+
+talkerRouter.post('/talker', async (req, res) => {
+    const { authorization } = req.headers;
+    const { name, age, talk } = req.body;
+    const { watchedAt, rate } = talk;
+    verifyAuthorization(authorization);
+    verifyNameAndAge(name, age);
+    verifyTalk(talk);
+    verifyWatchedAt(watchedAt);
+    verifyRate(rate);
+    if (resultVerify[0] !== 0) {
+        res.status(400).json({ message: resultVerify[1] });
+    }
+    res.status(201).json(req.body);
+});
 
 module.exports = talkerRouter;
