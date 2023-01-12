5a86abca751178bee20e7c7a83499a943af1cb35
HiagoP <hiagoperonip@gmail.com>
2023-01-11 21:27:00 -0300

    Requisito 02 pronto

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -10,4 +10,14 @@ talkerRouter.get('/talker', async (req, res) => {
     return res.status(200).json(data);
 });
 
+talkerRouter.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const data = await readFile(filePath);
+    const result = data.find((talker) => talker.id === Number(id));
+    if (!result) {
+        return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(200).json(result);
+});
+
 module.exports = talkerRouter;
